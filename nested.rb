50614cf099eef37165ae213a3f37a60172ed280a
Mark Stone <markwstone85@gmail.com>
2019-09-04 02:42:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -94,7 +94,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie]:languages
+     programmer_hash[:languages] = "C"
 end
 
 def adding_matz
